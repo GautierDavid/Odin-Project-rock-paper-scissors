cedc2de965db972d4d036692074ca50e59f03cc2
Gautier DAVID <gautier.d.91@gmail.com>
2022-08-02 15:32:34 +0200

    fonction choix user et computer

--- main.js
+++ main.js
@@ -1,17 +1,85 @@
 
+let computerChoice;
+let playerSlection;
+let playerVictoryCount = 0;
+let computerVictoryCount = 0;
+let choiceArr = ["rock", "paper", "scissors"]
+
+
+let choiceButton = document.querySelectorAll('.choice');
+
+choiceButton.forEach(possibilities => {
+    possibilities.addEventListener('click', playRound);
+})
+
+
+
 function getComputerChoice() {
-    let randomNumber = Math.floor(Math.random() * 3) + 1;
-    if(randomNumber === 1) return "Rock"
-    else if(randomNumber === 2) return "Paper"
-    else return "Scissors"
+    return choiceArr[Math.floor(Math.random() * choiceArr.length)]
 }
 
 
-function playRound(playerSelection, computerSelection) {
-    if(playerSelection.toLowerCase() === "rock" && computerSelection === "Paper" || playerSelection.toLowerCase() === "paper" && computerSelection === "Scissors" || playerSelection.toLowerCase() === "scissors" && computerSelection === "Rock") return "You lose! " + computerSelection + " beats " + playerSelection
-    else return "You win! " + playerSelection + " beats " + computerSelection
+
+
+function playRound(playerSelection, computerChoice) {
+    
+    playerSelection = event.currentTarget.id;
+    computerChoice = getComputerChoice()
+
+    if (playerSelection === "rock" && computerChoice === "paper" || playerSelection === "paper" && computerChoice === "scissors" || playerSelection === "scissors" && computerChoice === "rock") {
+        console.log("You lose! " + playerSelection + " is beaten by " + computerChoice)
+        computerVictoryCount ++
+
+    } else if (playerSelection === "paper" && computerChoice === "rock" || playerSelection === "scissors" && computerChoice === "paper" || playerSelection === "rock" && computerChoice === "scissors") {
+        console.log("You win! " + playerSelection + " beats " + computerChoice) 
+        playerVictoryCount ++
+
+    } else if (playerSelection === computerChoice) { 
+        console.log("It's a Tie!")
+
+    } else {
+        console.log("You made a mistake on your choice")
+    }
+
+    refreshScore(playerVictoryCount, computerVictoryCount)
 }
 
-const playerSelection = "Rock";
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+
+function refreshScore(playerScore, computerScore) {
+    
+}
+
+
+
+
+// function playRound(playerSelection, computerSelection, playerCount, computerCount) {
+    
+//     if(playerSelection.toLowerCase() === "rock" && computerSelection === "Paper" || playerSelection.toLowerCase() === "paper" && computerSelection === "Scissors" || playerSelection.toLowerCase() === "scissors" && computerSelection === "Rock") {
+//         console.log("You lose! " + computerSelection + " beats " + playerSelection)
+        
+//         return computerCount ++ 
+        
+//     } else  {
+//         console.log("You win! " + playerSelection + " beats " + computerSelection) 
+//         return playerCount ++
+//     }
+// }
+
+// function game() {
+
+//     for(let i = 0; i < 5; i++) {
+//         const playerSelection = prompt("What is your choice between rock, paper and scissors ?");
+//         const computerSelection = getComputerChoice();
+//         console.log(computerSelection, i)
+//         playRound(playerSelection, computerSelection, playerCount, computerCount)
+//     }
+
+//     if(playerCount > computerCount) {
+//         console.log("You win the game " + playerCount + " vs " + computerCount + "!")
+//     } else {
+//         console.log("You lose the game " + playerCount + " vs " + computerCount + "...")
+//     } 
+// }
+
+// game()
+
