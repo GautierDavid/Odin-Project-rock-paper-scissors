7b71e6388ae0f3e3bf676b5bc365dcbf14caaa84
Gautier DAVID <gautier.d.91@gmail.com>
2022-08-04 17:11:25 +0200

    refactorisation of the differents function

--- main.js
+++ main.js
@@ -5,97 +5,71 @@ let playerVictoryCount = 0;
 let computerVictoryCount = 0;
 let choiceArr = ["rock", "paper", "scissors"]
 
-let pUser = document.querySelector('.user-score');
-console.log(pUser)
-let pComputer = document.querySelector('.computer-score');
+let choiceButton = document.querySelectorAll('.choice'); // get the button
+let resultContainer = document.querySelector('.result'); // get the div where we'll show the result of a round
+let pUser = document.querySelector('.user-score'); // get the p for user
+let pComputer = document.querySelector('.computer-score'); // get the p for computer
 
 
-let choiceButton = document.querySelectorAll('.choice');
-
+// add event listener on all button
 choiceButton.forEach(possibilities => {
     possibilities.addEventListener('click', playRound);
 })
 
 
-
+/**
+ * function the computer choice thanks to a random method applied on an arr that contains the possibilities
+ * @returns 
+ */
 function getComputerChoice() {
     return choiceArr[Math.floor(Math.random() * choiceArr.length)]
 }
 
 
 
-
+/**
+ * Function to see who win the round and show it
+ * @param {*} playerSelection 
+ * @param {*} computerChoice 
+ */
 function playRound(playerSelection, computerChoice) {
     
-    playerSelection = event.currentTarget.id;
-    computerChoice = getComputerChoice()
+    playerSelection = event.currentTarget.id; // the id that start the event
+    computerChoice = getComputerChoice(); // call the function to get the computer choice
 
+    
     if (playerSelection === "rock" && computerChoice === "paper" || playerSelection === "paper" && computerChoice === "scissors" || playerSelection === "scissors" && computerChoice === "rock") {
-        console.log("You lose! " + playerSelection + " is beaten by " + computerChoice)
-        computerVictoryCount ++
-
-    } else if (playerSelection === "paper" && computerChoice === "rock" || playerSelection === "scissors" && computerChoice === "paper" || playerSelection === "rock" && computerChoice === "scissors") {
-        console.log("You win! " + playerSelection + " beats " + computerChoice) 
-        playerVictoryCount ++
-        console.log(playerVictoryCount)
-
+        resultContainer.innerHTML = `<p>You lose! ${playerSelection} is beaten by ${computerChoice}</p>`;
+        computerVictoryCount ++;
     } else if (playerSelection === computerChoice) { 
-        console.log("It's a Tie!")
-
+        resultContainer.innerHTML = `<p>It's a Tie!</p>`;
     } else {
-        console.log("You made a mistake on your choice")
+        resultContainer.innerHTML = `<p>You win! ${playerSelection} beats ${computerChoice}</p>`;
+        playerVictoryCount ++;
     }
-
+    
     refreshScore(playerVictoryCount, computerVictoryCount)
 }
 
 
 function refreshScore(playerScore, computerScore) {
-    console.log(playerScore)
-
+    
     if(playerScore < 5 && computerScore < 5) {
-        pUser.innerHTML = "My score: " + playerScore;
-        pComputer.innerHTML = "Computer score : " + computerScore;
-    } else if(playerScore === 5) {
-        pUser.innerHTML = "Congratulations, you win the game " + playerScore + " to " + computerScore + " !";
+        pUser.innerHTML = `My score: ${playerScore}`;
+        pComputer.innerHTML = `Computer score : ${computerScore}`;
+    } else if(playerScore > 5) {
+        pUser.innerHTML = `Congratulations, you win the game ${playerScore} to ${computerScore} !`;
+        choiceButton.forEach(possibilities => {
+            possibilities.removeEventListener('click', playRound);
+        })
         pComputer.style.visibility = "hidden";
-    } else if(computerScore === 5) {
-        pUser.innerHTML = "Too bad, you lose the game " + playerScore + " to " + computerScore + " !";
+        resultContainer.style.visibility = "hidden"
+    } else {
+        pUser.innerHTML = `Too bad, you lose the game ${playerScore} to ${computerScore} !`;
+        choiceButton.forEach(possibilities => {
+            possibilities.removeEventListener('click', playRound);
+        })
         pComputer.style.visibility = "hidden";
+        resultContainer.style.visibility = "hidden"
     }
 }
-
-
-
-
-// function playRound(playerSelection, computerSelection, playerCount, computerCount) {
-    
-//     if(playerSelection.toLowerCase() === "rock" && computerSelection === "Paper" || playerSelection.toLowerCase() === "paper" && computerSelection === "Scissors" || playerSelection.toLowerCase() === "scissors" && computerSelection === "Rock") {
-//         console.log("You lose! " + computerSelection + " beats " + playerSelection)
-        
-//         return computerCount ++ 
-        
-//     } else  {
-//         console.log("You win! " + playerSelection + " beats " + computerSelection) 
-//         return playerCount ++
-//     }
-// }
-
-// function game() {
-
-//     for(let i = 0; i < 5; i++) {
-//         const playerSelection = prompt("What is your choice between rock, paper and scissors ?");
-//         const computerSelection = getComputerChoice();
-//         console.log(computerSelection, i)
-//         playRound(playerSelection, computerSelection, playerCount, computerCount)
-//     }
-
-//     if(playerCount > computerCount) {
-//         console.log("You win the game " + playerCount + " vs " + computerCount + "!")
-//     } else {
-//         console.log("You lose the game " + playerCount + " vs " + computerCount + "...")
-//     } 
-// }
-
-// game()
-
